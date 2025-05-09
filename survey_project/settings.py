550e12637a6d2cb162eeb4024e5b2d55b876ad2b
ysmoraa <ysmoraa@gmail.com>
2025-05-08 19:56:11 -0500

    Confi archivos estáticos

--- survey_project/settings.py
+++ survey_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@x(-^8h0-5l8%lpw@!n377@udnwmoh)9f#bf7l5r)k3sd=_dl-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
